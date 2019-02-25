407e637edab6c173b47fa1ecdab74eb1ed1efa81
Andrew <sobacka@gmail.com>
2019-02-25 13:20:16 -0800

    Moved gamestate to heap to avoid errors

--- projects/sobacka/dominion/randomtestadventurer.c
+++ projects/sobacka/dominion/randomtestadventurer.c
@@ -39,38 +39,38 @@ int main(){
     gainCard(adventurer, &G, 2, currentPlayer);  // 3rd parameter is toFlag, 2 equals hand
 
     // Remove all cards from deck, by setting deck count to 0
-    G.deckCount[currentPlayer] = 0;
+    G->deckCount[currentPlayer] = 0;
     // Add random number of coppers to deck, inc deck count each time.
     int coppers = rand() % 11; //0 - 10 coppers to add to deck
     for(i = 0; i < coppers; i++){
-      G.deck[currentPlayer][i] = copper;
-      G.deckCount[currentPlayer]++;
+      G->deck[currentPlayer][i] = copper;
+      G->deckCount[currentPlayer]++;
     }
     // Add random number (0-10) of estates as well, then shuffle
     int estates = rand() % 11;
      for(i = 0; i < estates; i++){
-      G.deck[currentPlayer][i] = estate;
-      G.deckCount[currentPlayer]++;
+      G->deck[currentPlayer][i] = estate;
+      G->deckCount[currentPlayer]++;
     }
 
     shuffle(currentPlayer, &G);
 
     // Set random number of buys (0-10)
-    G.numBuys = rand() % 11;
+    G->numBuys = rand() % 11;
     // Set random number of actions (1-10). This must be at least 1 or else the card cannoted be played
-    G.numActions = (rand() % 10) + 1;
+    G->numActions = (rand() % 10) + 1;
 
     // Get values to be compared later
-    int testDeckCount = G.deckCount[currentPlayer];
-    int testHandCount = G.handCount[currentPlayer];
-    int testActions = G.numActions;
-    int testBuys = G.numBuys;
-    int testPlayed = G.playedCardCount;
-    int testCoins = G.coins;
+    int testDeckCount = G->deckCount[currentPlayer];
+    int testHandCount = G->handCount[currentPlayer];
+    int testActions = G->numActions;
+    int testBuys = G->numBuys;
+    int testPlayed = G->playedCardCount;
+    int testCoins = G->coins;
 
     int aPosition; // aPosition used to mark the adventurer card position
-    for (i = 0; i < G.handCount[currentPlayer]; i++){
-      if(G.hand[currentPlayer][i] == adventurer){
+    for (i = 0; i < G->handCount[currentPlayer]; i++){
+      if(G->hand[currentPlayer][i] == adventurer){
         aPosition = i;
         break;
       }
@@ -84,16 +84,16 @@ int main(){
 
 
     if(testDeckCount >= 2){
-      assertTrue(G.deckCount[currentPlayer] <= testDeckCount - 2, __LINE__, failFlag); // At least two cards drawn from deck
-      assertTrue(G.handCount[currentPlayer] >= testHandCount + 1, __LINE__, failFlag); // One card played and at least two drawn
+      assertTrue(G->deckCount[currentPlayer] <= testDeckCount - 2, __LINE__, failFlag); // At least two cards drawn from deck
+      assertTrue(G->handCount[currentPlayer] >= testHandCount + 1, __LINE__, failFlag); // One card played and at least two drawn
     }
     else if(testDeckCount == 1){
-      assertTrue(G.deckCount[currentPlayer] <= testDeckCount - 1, __LINE__, failFlag); // At least one drawn from deck
-      assertTrue(G.handCount[currentPlayer] >= testHandCount, __LINE__, failFlag); // One card is played, and one always drawn, which evens out
+      assertTrue(G->deckCount[currentPlayer] <= testDeckCount - 1, __LINE__, failFlag); // At least one drawn from deck
+      assertTrue(G->handCount[currentPlayer] >= testHandCount, __LINE__, failFlag); // One card is played, and one always drawn, which evens out
     }
     else if(testDeckCount == 0){
-      assertTrue(G.deckCount[currentPlayer] <= testDeckCount, __LINE__, failFlag);
-      assertTrue(G.handCount[currentPlayer] >= testHandCount - 1, __LINE__, failFlag); //One card is played, none drawn
+      assertTrue(G->deckCount[currentPlayer] <= testDeckCount, __LINE__, failFlag);
+      assertTrue(G->handCount[currentPlayer] >= testHandCount - 1, __LINE__, failFlag); //One card is played, none drawn
     }
     // This line should never be called
     else{
@@ -102,28 +102,28 @@ int main(){
 
     // Test that if there were two+ coppers in deck before, that the hand now contains exactly two more coppers
     if (coppers >= 2){
-      assertTrue(G.coins == testCoins + 2, __LINE__, failFlag);
+      assertTrue(G->coins == testCoins + 2, __LINE__, failFlag);
     }
     // Only one copper
     else if(coppers == 1){
-      assertTrue(G.coins == testCoins + 1, __LINE__, failFlag);
+      assertTrue(G->coins == testCoins + 1, __LINE__, failFlag);
     }
     // No coppers
     else if (coppers == 0){
-      assertTrue(G.coins == testCoins, __LINE__, failFlag);
+      assertTrue(G->coins == testCoins, __LINE__, failFlag);
     }
     else {
       assertTrue(0, __LINE__, failFlag);
     }
 
     // Actions should be one less than previously
-    assertTrue(G.numActions = testActions - 1, __LINE__, failFlag);
+    assertTrue(G->numActions = testActions - 1, __LINE__, failFlag);
 
     // Buys should not be affected by this card
-    assertTrue(G.numBuys == testBuys + 2, __LINE__, failFlag);
+    assertTrue(G->numBuys == testBuys + 2, __LINE__, failFlag);
 
     // The number of cards played should have increased by 1
-    assertTrue(G.playedCardCount == testPlayed + 1, __LINE__, failFlag);
+    assertTrue(G->playedCardCount == testPlayed + 1, __LINE__, failFlag);
 
     // Current player should not change
     assertTrue(whoseTurn(&G) == currentPlayer, __LINE__, failFlag);
@@ -155,31 +155,31 @@ void assertTrue(int value, int line, int* failFlag){
     // Set deck to be what I want
     int i;
     for (i = 0; i < MAX_DECK; i++){
-      G.deck[currentPlayer][i] = -1;
+      G->deck[currentPlayer][i] = -1;
     }
-    G.deck[currentPlayer][0] = estate;
-    G.deck[currentPlayer][1] = copper;
-    G.deck[currentPlayer][2] = estate;
-    G.deck[currentPlayer][3] = copper;
-    G.deck[currentPlayer][4] = copper;
-  G.deckCount[currentPlayer] = 5;
-    G.discardCount[currentPlayer] = 0;
+    G->deck[currentPlayer][0] = estate;
+    G->deck[currentPlayer][1] = copper;
+    G->deck[currentPlayer][2] = estate;
+    G->deck[currentPlayer][3] = copper;
+    G->deck[currentPlayer][4] = copper;
+  G->deckCount[currentPlayer] = 5;
+    G->discardCount[currentPlayer] = 0;
 
     // Set hand to be what I want
     for (i = 0; i < MAX_HAND; i++){
-      G.hand[currentPlayer][i] = -1;
+      G->hand[currentPlayer][i] = -1;
     }
-    G.hand[currentPlayer][0] = adventurer;
-  G.handCount[currentPlayer] = 1;
+    G->hand[currentPlayer][0] = adventurer;
+  G->handCount[currentPlayer] = 1;
 
     // Play Adventurer
     playCard(0, 0, 0, 0, &G);  //First parameter is the position of the adventurer in the hand (0)
 
     // Check discard pile
-    assertTrue(G.discardCount[currentPlayer] == 1, __LINE__, failFlag);
+    assertTrue(G->discardCount[currentPlayer] == 1, __LINE__, failFlag);
 
     // There should only be two cards in the hand, both coppers
-    assertTrue(G.handCount[currentPlayer] == 2, __LINE__, failFlag);
-    assertTrue(G.hand[currentPlayer][0] == copper, __LINE__, failFlag);
-    assertTrue(G.hand[currentPlayer][1] == copper, __LINE__, failFlag);
+    assertTrue(G->handCount[currentPlayer] == 2, __LINE__, failFlag);
+    assertTrue(G->hand[currentPlayer][0] == copper, __LINE__, failFlag);
+    assertTrue(G->hand[currentPlayer][1] == copper, __LINE__, failFlag);
 */
--- projects/sobacka/dominion/randomtestcard2.c
+++ projects/sobacka/dominion/randomtestcard2.c
@@ -39,29 +39,29 @@ int main(){
     gainCard(smithy, &G, 2, currentPlayer);  // 3rd parameter is toFlag, 2 equals hand
 
     // Remove all cards from deck, by setting deck count to 0
-    G.deckCount[currentPlayer] = 0;
+    G->deckCount[currentPlayer] = 0;
     // Add random number of coppers to deck, inc deck count each time.
     int coppers = rand() % 11; //0 - 10 coppers to add to deck
     for(i = 0; i < coppers; i++){
-      G.deck[currentPlayer][i] = copper;
-      G.deckCount[currentPlayer]++;
+      G->deck[currentPlayer][i] = copper;
+      G->deckCount[currentPlayer]++;
     }
 
     // Set random number of buys (0-10)
-    G.numBuys = rand() % 11;
+    G->numBuys = rand() % 11;
     // Set random number of actions (1-10). This must be at least 1 or else the card cannoted be played
-    G.numActions = (rand() % 10) + 1;
+    G->numActions = (rand() % 10) + 1;
 
     // Get values to be compared later
-    int testDeckCount = G.deckCount[currentPlayer];
-    int testHandCount = G.handCount[currentPlayer];
-    int testActions = G.numActions;
-    int testBuys = G.numBuys;
-    int testPlayed = G.playedCardCount;
+    int testDeckCount = G->deckCount[currentPlayer];
+    int testHandCount = G->handCount[currentPlayer];
+    int testActions = G->numActions;
+    int testBuys = G->numBuys;
+    int testPlayed = G->playedCardCount;
 
     int smithyPosition; // smithyPosition used to mark the smithy card position
-    for (i = 0; i < G.handCount[currentPlayer]; i++){
-      if(G.hand[currentPlayer][i] == village){
+    for (i = 0; i < G->handCount[currentPlayer]; i++){
+      if(G->hand[currentPlayer][i] == village){
         smithyPosition = i;
         break;
       }
@@ -75,24 +75,24 @@ int main(){
     // So long as there are enough cards in the deck, one card is removed from the hand, and three are added
     // Three are also removed from the deck
     if(testDeckCount >= 3){
-      assertTrue(G.deckCount[currentPlayer] == testDeckCount - 3, __LINE__, failFlag);
-      assertTrue(G.handCount[currentPlayer] == testHandCount + 2, __LINE__, failFlag);
+      assertTrue(G->deckCount[currentPlayer] == testDeckCount - 3, __LINE__, failFlag);
+      assertTrue(G->handCount[currentPlayer] == testHandCount + 2, __LINE__, failFlag);
     }
     // With only two cards in the deck, two are added to the hand and one is played, and the deck should now have no cards
     else if(testDeckCount == 2){
-      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
-      assertTrue(G.handCount[currentPlayer] == testHandCount + 1, __LINE__, failFlag);
+      assertTrue(G->deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G->handCount[currentPlayer] == testHandCount + 1, __LINE__, failFlag);
     }
     // If there is only one card in the deck, one card is played, one is added to the hand, and the deck should now be 0
     else if(testDeckCount == 1){
-      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
-      assertTrue(G.handCount[currentPlayer] == testHandCount, __LINE__, failFlag);
+      assertTrue(G->deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G->handCount[currentPlayer] == testHandCount, __LINE__, failFlag);
     }
     // In this case, the handcount should decrease, as one card has been played
     // However, the deck count should remain the same, as nothing was in it to draw from
     else if(testDeckCount == 0){
-      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
-      assertTrue(G.handCount[currentPlayer] == testHandCount - 1, __LINE__, failFlag);
+      assertTrue(G->deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G->handCount[currentPlayer] == testHandCount - 1, __LINE__, failFlag);
     }
     // This line should never be called
     else{
@@ -100,13 +100,13 @@ int main(){
     }
 
     // Actions should be one less than previously
-    assertTrue(G.numActions = testActions - 1, __LINE__, failFlag);
+    assertTrue(G->numActions = testActions - 1, __LINE__, failFlag);
 
     // Buys should not be affected by this card
-    assertTrue(G.numBuys == testBuys + 2, __LINE__, failFlag);
+    assertTrue(G->numBuys == testBuys + 2, __LINE__, failFlag);
 
     // The number of cards played should have increased by 1
-    assertTrue(G.playedCardCount == testPlayed + 1, __LINE__, failFlag);
+    assertTrue(G->playedCardCount == testPlayed + 1, __LINE__, failFlag);
 
     // Current player should not change
     assertTrue(whoseTurn(&G) == currentPlayer, __LINE__, failFlag);

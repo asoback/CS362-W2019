db0db9d260a44e283dff43230cf12e6d05dc2a17
asoback <31739297+asoback@users.noreply.github.com>
2019-02-24 18:22:20 -0600

    Added random test card files

---
+++ projects/sobacka/dominion/randomtestadventurer.c
@@ -0,0 +1,184 @@
+/*
+Andrew Soback
+Random Tester for Adventurer Card
+Assignment 4
+
+Adventurer draws cards, discards them if they are not treasure cards, and keeps treasures, until two treasures have been drawn.
+Last changed: 2/25/19
+*/
+
+#include "dominion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void assertTrue(int, int, int*);
+
+int main(){
+	int val = 0;
+	int *failFlag;
+	failFlag = &val;
+
+
+  srand(time(NULL));
+  //This test is just to make sure that my assert true works
+  assertTrue(true, __LINE__, failFlag);
+
+  // Run 10,000 random tests.
+  for (int _ = 0; _ < 10000; _++){
+    int i;
+    int seed = (rand() % 10) + 1;
+    int players = (rand() % 7) +  2; // 2 to 8 players 
+
+    int k[10] = {smithy, adventurer, council_room, village, minion, mine, cutpurse, sea_hag, tribute, ambassador};
+    struct gameState G;
+    initializeGame(players, k, seed, &G); // Game initialized with a random number of playes, as well as a random seed
+
+    int currentPlayer = whoseTurn(&G);
+    gainCard(adventurer, &G, 2, currentPlayer);  // 3rd parameter is toFlag, 2 equals hand
+
+    // Remove all cards from deck, by setting deck count to 0
+    G.deckCount[currentPlayer] = 0;
+    // Add random number of coppers to deck, inc deck count each time.
+    int coppers = rand() % 11; //0 - 10 coppers to add to deck
+    for(i = 0; i < coppers; i++){
+      G.deck[currentPlayer][i] = copper;
+      G.deckCount[currentPlayer]++;
+    }
+    // Add random number (0-10) of estates as well, then shuffle
+    int estates = rand() % 11;
+     for(i = 0; i < estates; i++){
+      G.deck[currentPlayer][i] = estate;
+      G.deckCount[currentPlayer]++;
+    }
+
+    shuffle(currentPlayer, G);
+
+    // Set random number of buys (0-10)
+    G.numBuys = rand() % 11;
+    // Set random number of actions (1-10). This must be at least 1 or else the card cannoted be played
+    G.numActions = (rand() % 10) + 1;
+
+    // Get values to be compared later
+    int testDeckCount = G.deckCount[currentPlayer];
+    int testHandCount = G.handCount[currentPlayer];
+    int testActions = G.numActions;
+    int testBuys = G.numBuys;
+    int testPlayed = G.playedCardCount;
+    int testCoins = G.coins;
+
+    int aPosition; // aPosition used to mark the adventurer card position
+    for (i = 0; i < G.handCount[currentPlayer]; i++){
+      if(G.hand[currentPlayer][i] == adventurer){
+        aPosition = i;
+        break;
+      }
+    }
+
+    // Test apparent successful run of card (return value of 0)
+    int res;
+    res = playCard(aPosition, 0, 0 ,0, &G);
+    assertTrue(res == 0, __LINE__, failFlag);
+
+
+
+    if(testDeckCount >= 2){
+      assertTrue(G.deckCount[currentPlayer] <= testDeckCount - 2, __LINE__, failFlag); // At least two cards drawn from deck
+      assertTrue(G.handCount[currentPlayer] >= testHandCount + 1, __LINE__, failFlag); // One card played and at least two drawn
+    }
+    else if(testDeckCount == 1){
+      assertTrue(G.deckCount[currentPlayer] <= testDeckCount - 1, __LINE__, failFlag); // At least one drawn from deck
+      assertTrue(G.handCount[currentPlayer] >= testHandCount, __LINE__, failFlag); // One card is played, and one always drawn, which evens out
+    }
+    else if(testDeckCount == 0){
+      assertTrue(G.deckCount[currentPlayer] <= testDeckCount, __LINE__, failFlag);
+      assertTrue(G.handCount[currentPlayer] >= testHandCount - 1, __LINE__, failFlag); //One card is played, none drawn
+    }
+    // This line should never be called
+    else{
+      assertTrue(false, __LINE__, failFlag);
+    }
+
+    // Test that if there were two+ coppers in deck before, that the hand now contains exactly two more coppers
+    if (coppers >= 2){
+      assertTrue(G.coins == testCoins + 2);
+    }
+    // Only one copper
+    else if(coppers == 1){
+      assertTrue(G.coins == testCoins + 1);
+    }
+    // No coppers
+    else if (coppers == 0){
+      assertTrue(G.coins == testCoins);
+    }
+    else {
+      assertTrue(false, __LINE__, failFlag);
+    }
+
+    // Actions should be one less than previously
+    assertTrue(G.numActions = testActions - 1, __LINE__, failFlag);
+
+    // Buys should not be affected by this card
+    assertTrue(G.numBuys] == testBuys + 2, __LINE__, failFlag);
+
+    // The number of cards played should have increased by 1
+    assertTrue(G.playedCardCount == testPlayed + 1, __LINE__, failFlag);
+
+    // Current player should not change
+    assertTrue(whoseTurn(&G) == currentPlayer);
+  }
+
+
+	if (*failFlag == 0) printf("TEST SUCCESSFULLY COMPLETED: Card Test 2 \n");
+  else printf("TEST FAILED: Card Test 2 \n");
+
+	return 0;
+}
+
+
+void assertTrue(int value, int line, int* failFlag){
+	if(value == 0){
+		printf("Error on line %d\n", line);
+		*failFlag = 1;
+	}
+}
+
+
+
+/*
+  int k[10] = {smithy, adventurer, council_room, village, minion, mine, cutpurse, sea_hag, tribute, ambassador};
+    struct gameState G;
+    initializeGame(2, k, 1, &G); //Two player game, with the above array of cards, random seed, and the gamestate struct
+
+    int currentPlayer = whoseTurn(&G);
+    // Set deck to be what I want
+    int i;
+    for (i = 0; i < MAX_DECK; i++){
+      G.deck[currentPlayer][i] = -1;
+    }
+    G.deck[currentPlayer][0] = estate;
+    G.deck[currentPlayer][1] = copper;
+    G.deck[currentPlayer][2] = estate;
+    G.deck[currentPlayer][3] = copper;
+    G.deck[currentPlayer][4] = copper;
+  G.deckCount[currentPlayer] = 5;
+    G.discardCount[currentPlayer] = 0;
+
+    // Set hand to be what I want
+    for (i = 0; i < MAX_HAND; i++){
+      G.hand[currentPlayer][i] = -1;
+    }
+    G.hand[currentPlayer][0] = adventurer;
+  G.handCount[currentPlayer] = 1;
+
+    // Play Adventurer
+    playCard(0, 0, 0, 0, &G);  //First parameter is the position of the adventurer in the hand (0)
+
+    // Check discard pile
+    assertTrue(G.discardCount[currentPlayer] == 1, __LINE__, failFlag);
+
+    // There should only be two cards in the hand, both coppers
+    assertTrue(G.handCount[currentPlayer] == 2, __LINE__, failFlag);
+    assertTrue(G.hand[currentPlayer][0] == copper, __LINE__, failFlag);
+    assertTrue(G.hand[currentPlayer][1] == copper, __LINE__, failFlag);
+*/
---
+++ projects/sobacka/dominion/randomtestcard2.c
@@ -0,0 +1,126 @@
+/*
+Andrew Soback
+Random Tester 2
+Assignment 4
+Tests Smithy
+
+Smithy gains the player 3 cards from their deck to their hand, when played.
+Last changed: 2/25/19
+*/
+
+#include "dominion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void assertTrue(int, int, int*);
+
+int main(){
+	int val = 0;
+	int *failFlag;
+	failFlag = &val;
+
+  srand(time(NULL));
+  //This test is just to make sure that my assert true works
+  assertTrue(true, __LINE__, failFlag);
+
+  // Run 10,000 random tests.
+  for (int _ = 0; _ < 10000; _++){
+    int i;
+    int seed = (rand() % 10) + 1;
+    int players = (rand() % 7) +  2; // 2 to 8 players 
+
+    int k[10] = {smithy, adventurer, council_room, village, minion, mine, cutpurse, sea_hag, tribute, ambassador};
+    struct gameState G;
+    initializeGame(players, k, seed, &G); // Game initialized with a random number of playes, as well as a random seed
+
+    int currentPlayer = whoseTurn(&G);
+    gainCard(smithy, &G, 2, currentPlayer);  // 3rd parameter is toFlag, 2 equals hand
+
+    // Remove all cards from deck, by setting deck count to 0
+    G.deckCount[currentPlayer] = 0;
+    // Add random number of coppers to deck, inc deck count each time.
+    int coppers = rand() % 11; //0 - 10 coppers to add to deck
+    for(i = 0; i < coppers; i++){
+      G.deck[currentPlayer][i] = copper;
+      G.deckCount[currentPlayer]++;
+    }
+
+    // Set random number of buys (0-10)
+    G.numBuys = rand() % 11;
+    // Set random number of actions (1-10). This must be at least 1 or else the card cannoted be played
+    G.numActions = (rand() % 10) + 1;
+
+    // Get values to be compared later
+    int testDeckCount = G.deckCount[currentPlayer];
+    int testHandCount = G.handCount[currentPlayer];
+    int testActions = G.numActions;
+    int testBuys = G.numBuys;
+    int testPlayed = G.playedCardCount;
+
+    int smithyPosition; // smithyPosition used to mark the smithy card position
+    for (i = 0; i < G.handCount[currentPlayer]; i++){
+      if(G.hand[currentPlayer][i] == village){
+        smithyPosition = i;
+        break;
+      }
+    }
+
+    // Test apparent successful run of card (return value of 0)
+    int res;
+    res = playCard(smithyPosition, 0, 0 ,0, &G);
+    assertTrue(res == 0, __LINE__, failFlag);
+
+    // So long as there are enough cards in the deck, one card is removed from the hand, and three are added
+    // Three are also removed from the deck
+    if(testDeckCount >= 3){
+      assertTrue(G.deckCount[currentPlayer] == testDeckCount - 3, __LINE__, failFlag);
+      assertTrue(G.handCount[currentPlayer] == testHandCount + 2, __LINE__, failFlag);
+    }
+    // With only two cards in the deck, two are added to the hand and one is played, and the deck should now have no cards
+    else if(testDeckCount == 2){
+      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G.handCount[currentPlayer] == testHandCount + 1, __LINE__, failFlag);
+    }
+    // If there is only one card in the deck, one card is played, one is added to the hand, and the deck should now be 0
+    else if(testDeckCount == 1){
+      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G.handCount[currentPlayer] == testHandCount, __LINE__, failFlag);
+    }
+    // In this case, the handcount should decrease, as one card has been played
+    // However, the deck count should remain the same, as nothing was in it to draw from
+    else if(testDeckCount == 0){
+      assertTrue(G.deckCount[currentPlayer] == 0, __LINE__, failFlag);
+      assertTrue(G.handCount[currentPlayer] == testHandCount - 1, __LINE__, failFlag);
+    }
+    // This line should never be called
+    else{
+      assertTrue(false, __LINE__, failFlag);
+    }
+
+    // Actions should be one less than previously
+    assertTrue(G.numActions = testActions - 1, __LINE__, failFlag);
+
+    // Buys should not be affected by this card
+    assertTrue(G.numBuys] == testBuys + 2, __LINE__, failFlag);
+
+    // The number of cards played should have increased by 1
+    assertTrue(G.playedCardCount == testPlayed + 1, __LINE__, failFlag);
+
+    // Current player should not change
+    assertTrue(whoseTurn(&G) == currentPlayer);
+  }
+
+	if (*failFlag == 0) printf("TEST SUCCESSFULLY COMPLETED: Card Test 1 \n");
+  else printf("TEST FAILED: Card Test 1 \n");
+
+	return 0;
+}
+
+
+void assertTrue(int value, int line, int* failFlag){
+	if(value == 0){
+		printf("Error on line %d\n", line);
+		*failFlag = 1;
+	}
+}
